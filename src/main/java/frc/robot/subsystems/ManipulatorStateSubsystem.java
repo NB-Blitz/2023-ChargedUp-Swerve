72722d374ae124ea20367598f0fc01d2685463ca
leonardoGS-5148 <ajl13900@gmail.com>
2023-02-14 20:34:14 -0600

    manipulator state subsystem file and boilerplate code added

---
+++ src/main/java/frc/robot/subsystems/ManipulatorStateSubsystem.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ManipulatorStateSubsystem extends SubsystemBase {
+
+    public ManipulatorStateSubsystem() {
+        
+    }
+
+    public void setHome() {
+
+    }
+
+    public void setFloor() {
+
+    }
+
+    public void setTwo() {
+
+    }
+
+    public void setThree() {
+
+    }
+
+    public void setPlayerArea() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        
+    }
+}
